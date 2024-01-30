bd67755b4a86ea0653690c8df345d1e404d1ff95
Alexandr Smirnov <bearmd93@gmail.com>
2024-01-30 23:22:11 +0200

    even odd random number

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,9 @@
 // code
+function evenOdd() {
+  let number = Math.floor(Math.random() * 100);
+  return number % 2 === 0
+    ? `${number} :Это четное число`
+    : `${number} :Это нечетное число`;
+}
+
+console.log(evenOdd());
